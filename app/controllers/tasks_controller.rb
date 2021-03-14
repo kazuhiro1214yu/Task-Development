ef83ab5cca9b124c9f068b1bc6f7b007da988176
kazuhiro1214yu <hamu.santa+kazu@gmail.com>
2021-03-14 13:12:52 +0000

    c6-task_index-finish

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,6 +5,7 @@ class TasksController < ApplicationController
   # c5 indexアクション（一覧表示）
   def index
     @tasks = @user.tasks
+    # ↑@userは「current_user」でも代用可能。（session_helper.rb　でcurrent_user を定義しているため）
   end
   
   def new
@@ -12,16 +13,19 @@ class TasksController < ApplicationController
   end 
   
   def create
-    @task = Task.new(task_params)
+    @task = @user.tasks.build(task_params)
+    # ↑@userは「current_user」でも代用可能。（session_helper.rb　でcurrent_user を定義しているため）
     if @task.save
-      redirect_to @user
+      flash[:success] = "タスク新規登録しました"
+      redirect_to user_tasks_url @user
     else
+      flash.now[:danger] = "タスク新規登録失敗しました"
       render :new
     end 
-    
     # 補足　buildによるtaskの保存方法※今はnewで代用可能らしい
     # @task = @user.tasks.build(task_params)
   end 
+# ★保存失敗した場合、直前に入力した内容が全て消える。直前の入力内容を残すコードが記述できていない★
   
   
   # 以下、繰り返し利用するメソッドを記述する
@@ -33,6 +37,6 @@ class TasksController < ApplicationController
     end 
     
     def task_params
-      params.require(:task).premit(:title, :detail,)
+      params.require(:task).permit(:title, :detail,)
     end 
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user
+  
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :detail, presence: true, length: { maximum: 200 }
+  
 end
