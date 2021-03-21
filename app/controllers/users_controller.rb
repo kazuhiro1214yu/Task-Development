b08e56b914160d62c991ca995f2847ae11d10b60
kazuhiro1214yu <hamu.santa+kazu@gmail.com>
2021-03-21 13:36:51 +0000

    D5-paginate-working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   
+  def index
+    @users = User.all
+  end 
   
   def show
   end
