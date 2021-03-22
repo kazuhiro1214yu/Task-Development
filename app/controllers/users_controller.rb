4af12e4f75cfa17a01c1b2b30a8571f840b84930
kazuhiro1214yu <hamu.santa+kazu@gmail.com>
2021-03-22 01:27:58 +0000

    D8-finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
+  before_action :currect_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
   
   def index
     @users = User.paginate(page: params[:page], per_page: 20)
@@ -39,6 +42,11 @@ class UsersController < ApplicationController
     end
   end
   
+  def destroy
+    @user.destroy
+    flash[:success] = "#{@user.name}のデータを削除しました。"
+    redirect_to users_url
+  end 
   
   
   private
@@ -51,5 +59,18 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
     end 
     
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
+    end 
     
+    def correct_user
+      redirect_to(root_url) unless current_user?(@user)
+    end 
+    
+    def admin_user
+      redirect_to root_url unless current_user.admin?
+    end 
 end
